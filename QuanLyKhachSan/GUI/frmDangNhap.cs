17d0123019f9495cb6f8359061691037a3c569e1
fgnnhanpro <nhangaren2605@gmail.com>
2024-07-18 00:38:24 +0700

    Khoi tao project ung dung windows form va thiet ke form dang nhap

---
+++ QuanLyKhachSan/GUI/frmDangNhap.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    public partial class frmDangNhap : Form
+    {
+        public frmDangNhap()
+        {
+            InitializeComponent();
+            this.Load += FrmDangNhap_Load;
+        }
+
+        private void FrmDangNhap_Load(object sender, EventArgs e)
+        {
+                panel1.BackColor = Color.FromArgb(100, 0, 0, 0);
+                button1.BackColor = Color.FromArgb(100, 0, 0, 0);
+        }
+    }
+}
