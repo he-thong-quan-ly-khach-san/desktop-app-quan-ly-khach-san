5dead720a32f76851ad7a8c9ed5dc161daddf414
LeHongNhan <nhangaren2605@gmail.com>
2024-08-03 00:44:47 +0700

    Test thu vien ma hoa mat khau

--- QuanLyKhachSan/DAL/NguoiDungDAL.cs
+++ QuanLyKhachSan/DAL/NguoiDungDAL.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.Linq;
 using System.Data;
+using BCrypt.Net;
 using DTO;
 namespace DAL
 {
@@ -12,7 +13,20 @@ namespace DAL
     {
         QLKSDataContext qlks = new QLKSDataContext();
         public NguoiDungDAL() { }
-        public List<QL_NGUOIDUNG> GetNguoiDungs() { return qlks.QL_NGUOIDUNGs.Select(nd => nd).ToList<QL_NGUOIDUNG>(); }
+        public List<QL_NGUOIDUNG> GetNguoiDungs() 
+        {
+            List<QL_NGUOIDUNG> lstND = qlks.QL_NGUOIDUNGs.Select(nd => nd).ToList<QL_NGUOIDUNG>();
+            List<QL_NGUOIDUNG> lstNDMaHoa = new List<QL_NGUOIDUNG>();
+            foreach (QL_NGUOIDUNG nd in lstND)
+            {
+
+                //truyen mat khau va cost vao ham ma hoa, sau do ham se tra ve 1 string ngoằng ngoèo
+                string mkMaHoa = BCrypt.Net.BCrypt.HashPassword(nd.MATKHAU, 10);
+                nd.MATKHAU = mkMaHoa;
+                lstNDMaHoa.Add(nd);
+            }
+            return lstNDMaHoa;
+        }
         public void ThemNguoiDungDAL(QL_NGUOIDUNG nguoiDung)
         {
             qlks.QL_NGUOIDUNGs.InsertOnSubmit(nguoiDung);
